e2280c21d364e21cf3f779763f53c3097e705c1c
James Rogers <jarogers095@gmail.com>
2020-01-26 09:59:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,18 @@ def batch_badge_creator(array_of_names)
 end
 
 def assign_rooms(array_of_speakers)
+  array_of_room_messages = []
+  array_of_speakers.each_with_index do |speaker,index| array_of_room_messages.push("Hello, #{speaker}! You'll be assigned to room #{index+1}")
+  end
+  return array_of_room_messages
+end
+
+def printer(names)
+  batch_badge_creator(names).each do |badge|
+    puts badge
+  end
   
+  assign_rooms(names).each do |message|
+    puts message
+  end
+end
