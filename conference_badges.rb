3198f06a5dd9fd3c6285d3d902b57093379bf5c2
James Rogers <jarogers095@gmail.com>
2020-01-26 09:48:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(attendee)
+  return "Hello, my name is #{attendee}."
+end
+
+def batch_badge_creator(array_of_names)
+  return array_of_names.collect {
+    |name| badge_maker(name)
+  }
+end
+
+def assign_rooms(array_of_speakers)
+  
